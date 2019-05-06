c2c7d22ed1514d855d28f5f34ee46f208d4f854d
Oliver Lohmann <oliver@lohmann.io>
2019-05-06 19:45:19 +0200

    Rename backend network access rules param.

--- 00-tf-backend/main.tf
+++ 00-tf-backend/main.tf
@@ -1,6 +1,6 @@
 locals {
   prefix_snake = "${lower("${var.prefix}")}"
-  ip_rules     = "${length(var.network_access_rules) > 0 ? join(",", var.network_access_rules) : chomp(data.http.myip.body)}"
+  ip_rules     = "${length(var.tf_backend_network_access_rules) > 0 ? join(",", var.tf_backend_network_access_rules) : chomp(data.http.myip.body)}"
   hash_suffix  = "${lower(substr(sha256(azurerm_resource_group.rg.id), 0, 6))}"
 
   resource_group_name    = "${var.tf_backend_resource_group_name != "" ? var.tf_backend_resource_group_name : "${local.prefix_snake}-shared-tf-state-rg"}"
